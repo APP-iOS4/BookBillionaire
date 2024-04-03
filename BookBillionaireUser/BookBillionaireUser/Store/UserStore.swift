a8c5e75dc085d781a856e3ea6526e02560e0ee26
Junyoung Choi <148533329+DevJunnn@users.noreply.github.com>
2024-04-03 14:13:21 +0900

    Fix: Store 폴더 수정, UserStore(임시 파일) 생성

---
+++ BookBillionaireUser/BookBillionaireUser/Store/UserStore.swift
@@ -0,0 +1,14 @@
+//
+//  UserStore.swift
+//  BookBillionaireUser
+//
+//  Created by 최준영 on 4/3/24.
+//
+
+import SwiftUI
+import Foundation
+import BookBillionaireCore
+
+class UserStore: ObservableObject {
+    
+}
