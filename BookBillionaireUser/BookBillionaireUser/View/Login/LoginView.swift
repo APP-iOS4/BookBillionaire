2b0c831a80375ced6f777b272a076e72181a60cf
seungjaeyuu <152110747+seungjaeyuu@users.noreply.github.com>
2024-04-08 10:37:07 +0900

    feat: 로그인 레이아웃 정리

--- BookBillionaireUser/BookBillionaireUser/View/Login/LoginView.swift
+++ BookBillionaireUser/BookBillionaireUser/View/Login/LoginView.swift
@@ -9,83 +9,82 @@ struct LoginView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color.accentColor1
-                    .edgesIgnoringSafeArea(.all)
-                VStack {
-                    Image("logo")
-                        .frame(width: 100, height: 100)
-                        .padding(.top, 50)
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.white)
+        ZStack {
+            Color.accentColor1
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("logo")
+                    .frame(width: 100, height: 100)
+                    .padding(.top, 50)
+                ZStack {
+                    Rectangle()
+                        .fill(Color.white)
+                        .cornerRadius(10)
+                        .frame(width: 335, height: 250)
+                    VStack(spacing: 20) {
+                        TextField("email", text: $emailText)
+                            .padding()
+                            .background(.thinMaterial)
                             .cornerRadius(10)
-                            .frame(width: 335, height: 250)
-                        VStack(spacing: 20) {
-                            TextField("email", text: $emailText)
-                                .padding()
-                                .background(.thinMaterial)
-                                .cornerRadius(10)
-                                .textInputAutocapitalization(.never)
-                            SecureField("Password", text: $passwordText)
-                                .padding()
-                                .background(.thinMaterial)
-                                .cornerRadius(10)
-                                .textInputAutocapitalization(.never)
-                            
-                            HStack(spacing: 10) {
-                                Spacer()
-                                NavigationLink {
-                                    SignUpView()
-                                } label: {
-                                    HStack {
-                                        Text("회원가입")
-                                            .foregroundColor(.accentColor1)
-                                    }
+                            .textInputAutocapitalization(.never)
+                        SecureField("Password", text: $passwordText)
+                            .padding()
+                            .background(.thinMaterial)
+                            .cornerRadius(10)
+                            .textInputAutocapitalization(.never)
+                        
+                        HStack(spacing: 10) {
+                            Spacer()
+                            NavigationLink {
+                                SignUpView()
+                            } label: {
+                                HStack {
+                                    Text("회원가입")
+                                        .foregroundColor(.accentColor1)
                                 }
-                                Spacer()
-                                Button(action: {
-                                    signInProcessing = true
-                                    authViewModel.emailAuthLogIn(email: emailText, password: passwordText)
-                                }) {
-                                    ZStack {
-                                        Text("로그인")
-                                            .padding()
-                                            .background(Color.white)
-                                            .foregroundColor(.accentColor1)
-                                            .background(emailText.isEmpty || passwordText.isEmpty ? .gray : .red)
-                                            .cornerRadius(10)
-                                        
-                                        if signInProcessing {
-                                            ProgressView()
-                                        }
-                                    }
-                                }
-                                .disabled(emailText.isEmpty || passwordText.isEmpty ? true : false)
-                                Spacer()
                             }
-                            .padding(.top, 20)
+                            Spacer()
+                            Button(action: {
+                                signInProcessing = true
+                                authViewModel.emailAuthLogIn(email: emailText, password: passwordText)
+                            }) {
+                                ZStack {
+                                    Text("로그인")
+                                        .padding()
+                                        .background(Color.white)
+                                        .foregroundColor(.accentColor1)
+                                        .background(emailText.isEmpty || passwordText.isEmpty ? .gray : .red)
+                                        .cornerRadius(10)
+                                    
+                                    if signInProcessing {
+                                        ProgressView()
+                                    }
+                                }
+                            }
+                            .disabled(emailText.isEmpty || passwordText.isEmpty ? true : false)
+                            Spacer()
                         }
+                        .padding(.top, 20)
                     }
-                    .padding(.horizontal, 50)
-                    
-                    Text("Or")
-                        .foregroundColor(.white)
-                        .padding()
-                    
-                    Button(action: {
-                        // 구글 로그인 버튼 눌렀을 때의 동작
-
-                    }) {
-                        Image("SignInWithGoogle")
-                            .resizable()
-                            .frame(width: 335, height: 50)
-                    }
-                    .padding(.bottom, 10)
-                    Spacer()
                 }
+                .padding(.horizontal, 50)
+                
+                Text("Or")
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Button(action: {
+                    // 구글 로그인 버튼 눌렀을 때의 동작
+                    
+                }) {
+                    Image("SignInWithGoogle")
+                        .resizable()
+                        .frame(width: 335, height: 50)
+                }
+                .padding(.bottom, 10)
+                Spacer()
             }
         }
+        
     }
 }
