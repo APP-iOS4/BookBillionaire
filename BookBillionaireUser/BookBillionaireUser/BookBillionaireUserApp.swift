07d90c721c31780325fbbb0686dac67e72801ceb
seungjaeyuu <152110747+seungjaeyuu@users.noreply.github.com>
2024-04-08 09:25:21 +0900

    로그인 관련 머지오류 수정

--- BookBillionaireUser/BookBillionaireUser/BookBillionaireUserApp.swift
+++ BookBillionaireUser/BookBillionaireUser/BookBillionaireUserApp.swift
@@ -29,9 +29,9 @@ struct BookBillionaireUserApp: App {
     
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            ContentView()
 //            LoginView()
-//                .environmentObject(authViewModel)
+                .environmentObject(authViewModel)
         }
     }
 }
--- BookBillionaireUser/BookBillionaireUser/View/Login/LoginView.swift
+++ BookBillionaireUser/BookBillionaireUser/View/Login/LoginView.swift
@@ -1,13 +1,8 @@
-//
-//  Login.swift
-//  BookBillionaire
-//
-//  Created by Seungjae Yu on 3/20/24.
-//
-
 import SwiftUI
+import GoogleSignIn
+import AuthenticationServices
 
-struct LoginView: View { 
+struct LoginView: View {
     @State var emailText: String = ""
     @State var passwordText: String = ""
     @State var signInProcessing: Bool = false
@@ -23,19 +18,27 @@ struct LoginView: View {
                         .frame(width: 200, height: 200)
                         .padding(.top, 50)
                     ZStack {
-                        Image("Rectangle 3")
+                        Rectangle()
+                            .fill(Color.white)
+                            .cornerRadius(10)
+                            .frame(width: 335, height: 300)
                         VStack(spacing: 20) {
                             TextField("email", text: $emailText)
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .padding(.horizontal, 20)
-                                .foregroundColor(Color(hex: 0x014073))
                                 .padding(.top, 50)
                                 .textInputAutocapitalization(.none)
+                                .cornerRadius(10)
+                                .background(.thinMaterial)
+                                .foregroundColor(.accentColor1)
+                                .textInputAutocapitalization(.never)
                                 .autocapitalization(.none)
                             SecureField("Password", text: $passwordText)
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .padding(.horizontal, 20)
-                                .foregroundColor(Color(hex: 0x014073))
+                                .cornerRadius(10)
+                                .background(.thinMaterial)
+                                .foregroundColor(.accentColor1)
                             
                             HStack(spacing: 20) {
                                 Spacer()
@@ -44,7 +47,7 @@ struct LoginView: View {
                                 } label: {
                                     HStack {
                                         Text("회원가입")
-                                            .foregroundColor(Color(hex: 0x014073))
+                                            .foregroundColor(.accentColor1)
                                     }
                                 }
                                 Spacer()
@@ -56,8 +59,8 @@ struct LoginView: View {
                                         Text("로그인")
                                             .padding()
                                             .background(Color.white)
-                                            .foregroundColor(Color(hex: 0x014073))
-                                            .background(emailText.isEmpty || passwordText.isEmpty == true ? .gray : .red)
+                                            .foregroundColor(.accentColor1)
+                                            .background(emailText.isEmpty || passwordText.isEmpty ? .gray : .red)
                                             .cornerRadius(10)
                                         
                                         if signInProcessing {
@@ -72,30 +75,23 @@ struct LoginView: View {
                         }
                     }
                     .padding(.horizontal, 50)
+                    
                     Text("Or")
                         .foregroundColor(.white)
                         .padding()
-                    Image("SignInWithGoogle")
+                    
+                    Button(action: {
+                        // 구글 로그인 버튼 눌렀을 때의 동작
+
+                    }) {
+                        Image("SignInWithGoogle")
+                            .resizable()
+                            .frame(width: 335, height: 50)
+                    }
+                    .padding(.bottom, 10)
                     Spacer()
                 }
             }
         }
     }
 }
-
-extension Color {
-    init(hex: UInt, alpha: Double = 1) {
-        self.init(
-            .sRGB,
-            red: Double((hex >> 16) & 0xFF) / 255.0,
-            green: Double((hex >> 8) & 0xFF) / 255.0,
-            blue: Double((hex) & 0xFF) / 255.0,
-            opacity: alpha
-        )
-    }
-}
-
-#Preview {
-    LoginView()
-}
-
