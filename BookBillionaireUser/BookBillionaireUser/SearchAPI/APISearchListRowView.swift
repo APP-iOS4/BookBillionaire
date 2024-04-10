0853538dfa0ff96eac66c1f8f880bfc6eaf9cd28
WooSJ <madness1102@gmail.com>
2024-04-10 22:18:39 +0900

    Refactor: APISearchListRowView
    - 기존 뷰들을 Button의 label로 포함
    - Checkmark 추가
    - 책을 누르면 sheet가 내려가는 기능

--- BookBillionaireUser/BookBillionaireUser/SearchAPI/APISearchListRowView.swift
+++ BookBillionaireUser/BookBillionaireUser/SearchAPI/APISearchListRowView.swift
@@ -10,31 +10,44 @@ import BookBillionaireCore
 
 struct APISearchListRowView: View {
     var book : SearchBook
+    @State var selectedBooks: Bool = false
+    @Binding var isShowing: Bool
+    
     var body: some View {
-            HStack {
-                AsyncImage(url: URL(string: book.thumbnail)){ image in
-                    image.resizable()
-                        .frame(maxWidth: 80, maxHeight: 120)
-                        .aspectRatio(contentMode: .fill)
-                } placeholder: {
-                    ProgressView()
-                }
-                .padding(.trailing)
-                VStack(alignment:.leading) {
-                    Text(book.title)
-                        .font(.subheadline)
-                        .padding(.top)
-                    Text("\(book.authors.joined()) \(book.translators.joined())")
-                        .font(.caption)
-                        .padding(.bottom, 10)
-                    Spacer()
-                    HStack(alignment: .bottom) {
-                        Text(book.publisher)
-                            .font(.caption)
+                Button {
+                    selectedBooks.toggle()
+                    isShowing.toggle()
+                } label: {
+                    HStack {
+                    AsyncImage(url: URL(string: book.thumbnail)){ image in
+                        image.resizable()
+                            .frame(maxWidth: 80, maxHeight: 120)
+                            .aspectRatio(contentMode: .fill)
+                    } placeholder: {
+                        ProgressView()
                     }
+                    .padding(.trailing)
+                    VStack(alignment:.leading) {
+                        Text(book.title)
+                            .font(.subheadline)
+                            .padding(.top)
+                        Text("\(book.authors.joined()) \(book.translators.joined())")
+                            .font(.caption)
+                            .padding(.bottom, 10)
+                        Spacer()
+                        HStack(alignment: .bottom) {
+                            Text(book.publisher)
+                                .font(.caption)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 30)
+                    Image(systemName: "checkmark.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundStyle(selectedBooks ? Color.green : Color.gray)
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 30)
             }
             .padding(.horizontal)
     }
