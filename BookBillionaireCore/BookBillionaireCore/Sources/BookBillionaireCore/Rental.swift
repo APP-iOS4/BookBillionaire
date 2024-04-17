3b8abbf4d7bd2779a3c0e3e0f40f630e30154747
UDI <145957641+UDI94@users.noreply.github.com>
2024-04-17 17:33:55 +0900

    Refactor: RentalService
    - Refactor: RentalService

--- BookBillionaireCore/BookBillionaireCore/Sources/BookBillionaireCore/Rental.swift
+++ BookBillionaireCore/BookBillionaireCore/Sources/BookBillionaireCore/Rental.swift
@@ -8,16 +8,26 @@
 import Foundation
 
 public struct Rental: Identifiable, Codable{
-    public var id: String = UUID().uuidString
+    public var id: String
     public var bookOwner: String
     public var bookBorrower: String?
     public var rentalStartDay: Date
     public var rentalEndDay: Date
     public var rentalTime: Date?
-    public var map: String?
+    public var map: String
     
-    public init(id: String, bookOwner: String, bookBorrower: String? = nil, rentalStartDay: Date, rentalEndDay: Date, rentalTime: Date? = nil, map: String? = nil) {
-        self.id = id
+    public init() {
+        self.id = UUID().uuidString
+        self.bookOwner = ""
+        self.bookBorrower = ""
+        self.rentalStartDay = Date()
+        self.rentalEndDay = Date()
+        self.rentalTime = Date()
+        self.map = ""
+    }
+    
+    public init(id: String, bookOwner: String, bookBorrower: String? = nil, rentalStartDay: Date, rentalEndDay: Date, rentalTime: Date? = nil, map: String) {
+        self.id = UUID().uuidString
         self.bookOwner = bookOwner
         self.bookBorrower = bookBorrower
         self.rentalStartDay = rentalStartDay
@@ -25,24 +35,4 @@ public struct Rental: Identifiable, Codable{
         self.rentalTime = rentalTime
         self.map = map
     }
-    
-    public init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: RentalCodingKeys.self)
-        self.id = try container.decode(String.self, forKey: .id)
-        self.bookOwner = try container.decode(String.self, forKey: .bookOwner)
-        self.bookBorrower = try container.decodeIfPresent(String.self, forKey: .bookBorrower)
-        self.rentalStartDay = try container.decode(Date.self, forKey: .rentalStartDay)
-        self.rentalEndDay = try container.decode(Date.self, forKey: .rentalEndDay)
-        self.rentalTime = try container.decodeIfPresent(Date.self, forKey: .rentalTime)
-        self.map = try container.decodeIfPresent(String.self, forKey: .map)
-    }
-}
-
-public enum RentalCodingKeys: String, CodingKey {
-    case id, bookOwner
-    case bookBorrower
-    case rentalStartDay
-    case rentalEndDay
-    case rentalTime
-    case map
 }
