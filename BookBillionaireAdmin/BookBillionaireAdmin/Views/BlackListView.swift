958e7c86561a2bf6a1780e33aadd0b554d2944c2
UDI <145957641+UDI94@users.noreply.github.com>
2024-04-23 14:29:16 +0900

    Feat: CoreModel
    - Feat: CoreModel

--- BookBillionaireAdmin/BookBillionaireAdmin/Views/BlackListView.swift
+++ BookBillionaireAdmin/BookBillionaireAdmin/Views/BlackListView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct BlackListView: View {
+    var topic: Topic
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    BlackListView()
+    NavigationStack{
+        BlackListView(topic: Topic(name: "신고 유저 관리", Icon: "exclamationmark.triangle.fill", topicTitle: .blackList))
+    }
 }
---
+++ BookBillionaireCore/BookBillionaireCore/Sources/BookBillionaireCore/BlackList.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by YUJIN JEON on 4/23/24.
+//
+
+import Foundation
+
+struct BlackList {
+    var id: String
+    var reporter: String
+    var reciever: String
+    var reason: String
+}
+
