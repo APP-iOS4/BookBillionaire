5c338a2128912e8664aae1863c3ad367c5e648da
WooSJ <madness1102@gmail.com>
2024-04-25 17:56:39 +0900

    Feat: QnAView 사진 첨부 기능 구현
    - PhotosPicker 사진 첨부 기능 구현
    - UploadPhoto 함수 구현(path 수정필요)

--- BookBillionaireAdmin/BookBillionaireAdmin/Views/QnAView.swift
+++ BookBillionaireAdmin/BookBillionaireAdmin/Views/QnAView.swift
@@ -6,30 +6,46 @@
 //
 
 import SwiftUI
+import PhotosUI
+import FirebaseStorage
 
 struct QnAView: View {
     var topic: Topic
     @State var qnaService = QnAService()
     @State var title: String = ""
     @State var content: String = ""
-
+    @State private var selectedImage: UIImage?
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var isShowingPhotosPicker: Bool = false
+    
     var body: some View {
-        VStack{
-            HStack{
+        VStack {
+            HStack {
                 Form {
                     Text("QnA 이미지 업로드")
                         .font(.title2).bold()
-                    ZStack{
+                    ZStack {
                         RoundedRectangle(cornerRadius: 5.0)
                             .stroke(Color.accentColor, lineWidth: 1)
-                        Image(systemName: "plus")
+                        
+                        Button {
+                            isShowingPhotosPicker.toggle()
+                        } label: {
+                            if let image = selectedImage {
+                                Image(uiImage: image)
+                                    .resizable()
+                            } else {
+                                Image(systemName: "plus")
+                            }
+                        }
                     }
-                }.formStyle(.columns)
-                    .padding(80)
+                }
+                .formStyle(.columns)
+                .padding(80)
                 Form {
                     Text("QnA 생성")
                         .font(.title2).bold()
-                    HStack{
+                    HStack {
                         TextField("제목을 입력하세요.", text: $title)
                             .padding()
                             .overlay(
@@ -57,15 +73,49 @@ struct QnAView: View {
                             title = ""
                             content = ""
                         }
-                    }.buttonStyle(AccentButtonStyle())
+                        uploadPhoto()
+                    }
+                    .buttonStyle(AccentButtonStyle())
                 }
                 .formStyle(.columns)
                 .padding(30)
             }
             
         }
+        .photosPicker(isPresented: $isShowingPhotosPicker, selection: $selectedItem, matching: .images)
+        .onChange(of: selectedItem) { _ in
+            Task {
+                if let selectedItem,
+                   let data = try? await selectedItem.loadTransferable(type: Data.self) {
+                    if let image = UIImage(data: data) {
+                        selectedImage = image
+                    }
+                }
+            }
+        }
         .navigationTitle(topic.name)
         .padding()
+    }
+    
+    private func uploadPhoto() {
+        guard let selectedItem = selectedItem else {
+            return
+        }
+        Task {
+            if let data = try? await selectedItem.loadTransferable(type: Data.self) {
+                let storageRef = Storage.storage().reference()
+                let path = "QnA/.jpg"
+                let fileRef = storageRef.child(path)
+                let _ = fileRef.putData(data, metadata: nil) { metadata, error in
+                    if error == nil && metadata != nil {
+                        // Handle successful upload
+                    } else if let error = error {
+                        // Handle unsuccessful upload
+                        print("Error uploading image: \(error)")
+                    }
+                }
+            }
+        }
     }
 }
 
