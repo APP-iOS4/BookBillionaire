8aeeafd64efef7711041735e3f1b333168351cbd
UDI <145957641+UDI94@users.noreply.github.com>
2024-04-25 17:17:15 +0900

    feat: QnA 등록화면
    - feat: QnA 등록화면

--- BookBillionaireAdmin/BookBillionaireAdmin/Views/QnAView.swift
+++ BookBillionaireAdmin/BookBillionaireAdmin/Views/QnAView.swift
@@ -12,32 +12,63 @@ struct QnAView: View {
     @State var qnaService = QnAService()
     @State var title: String = ""
     @State var content: String = ""
-    @State var qna: QnA = QnA(title: "", content: "")
+
     var body: some View {
         VStack{
             HStack{
-                    List{
-                        ForEach(qnaService.lists) { list in
-                            Text(list.title)
+                Form {
+                    Text("QnA 이미지 업로드")
+                        .font(.title2).bold()
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 5.0)
+                            .stroke(Color.accentColor, lineWidth: 1)
+                        Image(systemName: "plus")
+                    }
+                }.formStyle(.columns)
+                    .padding(80)
+                Form {
+                    Text("QnA 생성")
+                        .font(.title2).bold()
+                    HStack{
+                        TextField("제목을 입력하세요.", text: $title)
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color.accentColor, lineWidth: 1)
+                            )
+                    }
+                    .padding(.bottom)
+                    TextEditor(text: $content)
+                        .background(Color.primary.colorInvert())
+                        .frame(minHeight: 300, maxHeight: 400)
+                        .cornerRadius(5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.accentColor, lineWidth: 1)
+                                .frame(minHeight: 300, maxHeight: 400)
+                                .scrollContentBackground(.hidden)
+                            
+                        )
+                        .padding(.bottom)
+                    Button("QnA 생성") {
+                        if title != "" {
+                            let qna = QnA(title: title, content: content)
+                            qnaService.registerQnA(qna)
+                            title = ""
+                            content = ""
                         }
-                    }
-                VStack{
-                    TextField(
-                        "제목을 입력해주세요.", text: $title)
-                    .textFieldStyle(.roundedBorder)
-                    TextEditor(text: .constant("Placeholder"))
-                    Button("QnA 등록하기") {
-                        qnaService.registerQnA(qna)
-                    }
+                    }.buttonStyle(AccentButtonStyle())
                 }
+                .formStyle(.columns)
+                .padding(30)
             }
+            
         }
         .navigationTitle(topic.name)
-            .padding(50)
-            
-        
+        .padding()
     }
 }
+
 
 #Preview {
     QnAView(topic: Topic(name: "QnA관리", Icon: "doc.questionmark", topicTitle: .qna))
